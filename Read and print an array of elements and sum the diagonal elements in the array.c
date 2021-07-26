b1b9de64a2cf1b31a29ba4f7b6ed4e7687fa0047
ramana-vr4 <87382936+ramana-vr4@users.noreply.github.com>
2021-07-26 12:39:03 +0530

    Add files via upload

---
+++ Read and print an array of elements and sum the diagonal elements in the array.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main()
+{
+  int arr1[3][3],i,j;
+  
+       printf("\n\nRead a array of size 3x3 and print the matrix :\n");
+       printf("###################################################\n");  
+  
+ 
+    
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+	      printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr1[i][j]);
+      }
+  }  
+ 
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr1[i][j]);
+  }
+ printf("\n\n");
+{
+ 	int i, j, rows, columns, a[10][10], Sum = 0;
+  
+ 	printf("\n Please Enter Number of rows and columns  :  ");
+ 	scanf("%d %d", &i, &j);
+ 
+ 	printf("\n Please Enter the Matrix Elements \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0;columns < j;columns++)
+    	{
+      		scanf("%d", &a[rows][columns]);
+    	}
+  	}
+   	  
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		Sum = Sum + a[rows][rows];
+  	}
+ 
+ 	printf("\n The Sum of Diagonal Elements of a Matrix =  %d", Sum );
+
+ 	return 0;
+}
+}
